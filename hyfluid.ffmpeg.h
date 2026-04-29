8a36da742f5c148121d74f6c4bcd9ed32d42021a
Xayah-Hina <ycxiang14sep@gmail.com>
2026-04-29 09:22:24 +0800

    Update CMakeLists.txt to include SHA256 checksums for FFmpeg downloads and improve error handling

--- hyfluid.ffmpeg.h
+++ hyfluid.ffmpeg.h
@@ -1,6 +1,14 @@
 #ifndef HYFLUID_FFMPEG_H
 #define HYFLUID_FFMPEG_H
 
+#include <cerrno>
+#include <cinttypes>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+
 extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
